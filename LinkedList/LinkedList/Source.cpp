6bfa9ab37045bef8d77904ee6225bf32f3550da0
Hung <hphamviet83@gmail.com>
2018-01-14 14:22:33 +0700

    thêm main

---
+++ LinkedList/LinkedList/Source.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "LinkedList.h"
+#include "D_LinkedList.h"
+#include "Node.h"
+using namespace std;
+void main_linkedlist()
+{
+	LinkedList list;
+	for (int i = 0; i<3; i++)
+	{
+		list.Add(i);
+	}
+	list.Show();
+	cout << "\n";
+	list.AddFirst(-10);
+	list.AddAfter(-10, 2);
+	list.AddBefore(2, 3);
+	list.DeleteNode(1);
+	list.Show();
+}
+void main_D_linkedlist()
+{
+	D_LinkedList list;
+	for (size_t i = 0; i < 3; i++)
+	{
+		list.AddLast(i);
+	}
+	list.Show();
+	cout << "\n";
+	list.AddFirst(4);
+	list.AddLast(-1);
+	list.Show();
+}
+int main()
+{
+	cout << "Day la ham main" << endl;
+	main_D_linkedlist();
+	cout << "\n";
+	system("pause");
+	return 0;
+}
